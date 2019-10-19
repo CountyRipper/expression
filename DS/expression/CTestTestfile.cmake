8b98d7b9bd95d542193a978ef6f9f2bb0602d37f
‘Maverick <1097249217@qq.com>
2019-10-19 22:33:27 +0800

    DS_expression

---
+++ DS/expression/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/pc/Documents/cpp/expression
+# Build directory: C:/Users/pc/Documents/cpp/expression
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ DS/expression/expression.h
@@ -0,0 +1,37 @@
+
+#ifndef _EXPRESSION_H
+#define _EXPRESSION_H
+
+#include "stack1.h"
+#include <fstream>
+
+template<class Type>
+class expression{
+    private:
+        static bool Isoperator(char ch);//判断是否是操作符
+        static int Opri(char op);//判断优先级
+        static bool Isdigit(char ch);
+        //取出两个操作数,从堆栈中
+        static void get2Operands(stack<Type> &opnd,Type &a1,Type &a2);
+
+        //形成运算指令送入opnd栈中
+        static void DoOperator(stack<Type> &opnd,const Type &a1,char op,const Type &a2 );
+
+        //从键盘中读取输入流并且由中缀表达式转后缀表达式存入文件中
+        static void exchangexp(std::ofstream &outfile);
+
+        //计算后缀表达式值,来自于输入文件infile
+        static void calexpression(std::ifstream &infile);
+
+        
+    public:
+        //程序启动接口
+        static void Run();
+        //无参构造函数
+        expression();
+        //析构函数
+        virtual ~expression();
+};
+
+
+#endif
